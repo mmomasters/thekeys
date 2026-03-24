aabb4195429e0882dbab9c8280143d93166758dc
mmo-masters <info@mmo-masters.com>
2026-03-24 10:39:23 +0100

    Fix SMS: use form-encoded POST instead of JSON for SMSFactor API compatibility

--- SmoobuWebhook.php
+++ SmoobuWebhook.php
@@ -427,11 +427,11 @@ class SmoobuWebhook {
             $ch = curl_init($url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
             curl_setopt($ch, CURLOPT_HTTPHEADER, [
                 'Authorization: Bearer ' . $apiToken,
                 'Accept: application/json',
-                'Content-Type: application/json'
+                'Content-Type: application/x-www-form-urlencoded'
             ]);
             
             $response = curl_exec($ch);
