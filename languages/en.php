bcac666de0de6b22ae61ee4538490696352e6be0
Dominique Kakowczyk <mmomasters@users.noreply.github.com>
2026-04-01 20:39:06 +0200

    feat: add emojis to email messages

--- languages/en.php
+++ languages/en.php
@@ -6,7 +6,7 @@
 return [
     'subject' => 'Kolna Apartments access codes and information',
 
-    'message' => 'Dear {guest_name},
+    'sms_message' => 'Dear {guest_name},
 
 - Main building "Jana z Kolna 19" code is 1 + KEY + 5687
 - Lobby door code is 3256 + ENTER
@@ -16,7 +16,22 @@ Your apartment code will ONLY work between the check in and check out date and t
 Your check in: {arrival} from 15.00
 Your check out: {departure} until 12.00
 
-PARKING : A lot of parking spaces are located on the street near Kolna Apartments. Parking is free from 5pm to 8am and during weekends and holidays, pricing: https://spp.szczecin.pl/informacja/paid-parking-zone-pricing
+In case of any issue, please feel free to call us +48 91 819 99 65
+
+We wish you a very pleasant stay,
+Kolna Apartments',
+
+    'message' => 'Dear {guest_name},
+
+- Main building "Jana z Kolna 19" code is 1 + 🔑 + 5687
+- Lobby door code is 3256 + ENTER
+- Apartment {apartment_name} door code is {full_pin} + 🟦
+
+Your apartment code will ONLY work between the check in and check out date and time.
+Your check in: {arrival} from 15.00
+Your check out: {departure} until 12.00
+
+🅿️ PARKING : A lot of parking spaces are located on the street near Kolna Apartments. Parking is free from 5pm to 8am and during weekends and holidays, pricing: https://spp.szczecin.pl/informacja/paid-parking-zone-pricing
 
 In case of any issue, please feel free to call us +48 91 819 99 65
 
